75f852900e767808cb56f9b835d7c89df02166c4
Agostino De Marco <agostino.demarco@unina.it>
2016-05-16 18:57:49 +0200

    More on JSBSim, test_2

---
+++ JSBSim/python/init.py
@@ -0,0 +1,71 @@
+# set the style of the notebook
+from IPython.core.display import HTML
+def css_styling():
+    styles = open('./style/nbstyle.css', 'r').read()
+    return HTML(styles)
+css_styling()
+
+# load libraries and set plot parameters
+import math
+import numpy as np
+import scipy
+import tables as pt
+
+from scipy.interpolate import interp1d, interp2d, Rbf
+
+import h5py
+
+from sympy import *
+
+from IPython.display import display, Math, Latex, SVG
+
+from cycler import cycler
+
+import matplotlib.pyplot as plt
+from matplotlib.patches import Polygon
+from matplotlib.ticker import MultipleLocator, FormatStrFormatter
+
+from IPython.display import set_matplotlib_formats
+set_matplotlib_formats('pdf', 'png')
+plt.rcParams['savefig.dpi'] = 96
+
+plt.rcParams['figure.autolayout'] = False
+plt.rcParams['figure.figsize'] = 10, 12
+plt.rcParams['axes.labelsize'] = 18
+plt.rcParams['axes.titlesize'] = 20
+plt.rcParams['font.size'] = 16
+plt.rcParams['lines.linewidth'] = 2.0
+plt.rcParams['lines.markersize'] = 8
+plt.rcParams['legend.fontsize'] = 14
+
+plt.rcParams['text.usetex'] = True
+plt.rcParams['font.family'] = "serif"
+plt.rcParams['font.serif'] = "cm"
+# plt.rcParams['text.latex.preamble'] = "\usepackage{subdepth}, \usepackage{type1cm}"
+
+plt.rc('axes', 
+       prop_cycle=(
+           cycler('color', ['black', 'darkblue', 'red', 'green', 'blue', 'brown', 'orange', 'darkgreen'])
+           + cycler('linestyle',["-", "--", "-.", ":", ".", "h", "H","-."])
+           + cycler('dashes',[[10000,1], [10,5], [10,5,2,5], [3,3], [20,5,5,5], [30,5], [10,10], [2,5,5,3]])
+           )
+    )
+
+import tailer #pip install tailer
+
+import errno
+import fnmatch
+import os
+
+#----------------------------------------------------------
+
+def mkdir_p(path):
+    try:
+        os.makedirs(path, exist_ok=True)
+    except OSError as exc:  # Python >2.5
+        if exc.errno == errno.EEXIST and os.path.isdir(path):
+            pass
+        else:
+            raise
+
+#----------------------------------------------------------
